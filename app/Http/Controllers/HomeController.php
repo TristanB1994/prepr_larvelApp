585a2bd9efb32d215965dd2672af1b85a871e992
Ubuntu <ubuntu@ip-172-31-30-253.ca-central-1.compute.internal>
2020-09-05 23:33:59 +0000

    sync server routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-<<<<<<< HEAD
 	public function index () 
 	{
 		return view('index');
 	}
-=======
-	public function index() 
-	{	
-		return view('index');
-	}	
->>>>>>> tristan
 }
