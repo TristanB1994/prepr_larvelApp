526df333da988ffccc68a9b24f3110e6e05939d5
Ubuntu <ubuntu@ip-172-31-26-94.ca-central-1.compute.internal>
2020-09-06 08:14:27 +0000

    clean views, edit dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Locations;
 
 class DashboardController extends Controller
 {
 	public function make() 
 	{
+
+		$locations = Locations::all();
+
+		foreach($locations as $location) 
+		{
+
+			echo $location;
+
+		}die;	
+
 		return view('dashboard');
 	}
 }
